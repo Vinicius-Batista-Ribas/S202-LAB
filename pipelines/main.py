615733899efe24ddcf6174d66b298e172aa499fd
vinicius <vinicius_batista@gec.inatel.br>
2024-03-16 10:45:49 -0300

    criando a classe productanalyzer

--- pipelines/main.py
+++ pipelines/main.py
@@ -1,47 +1,21 @@
 from database import Database
+from productyAnalyser import ProductAnalyzer as pa
 from helper.writeAJson import writeAJson
 
 db = Database(database="mercado", collection="produtos")
 db.resetDatabase()
 
+analyzer = pa()
 # Vendas por dia
-result = db.collection.aggregate([
-    {"$group": {"_id": "$data_compra", "vendas_por_dia": {"$sum":1}}},
-])
-
+result = analyzer.VendasPorDia(db)
 writeAJson(result, "Total de vendas por dia")
 
-
-# 2- Produto mais vendido por compra
-result = db.collection.aggregate([
-    {"$unwind": "$produtos"},
-    {"$group": {"_id": "$produtos.descricao", "total_vendido": {"$sum": "$produtos.quantidade"}}},
-    {"$sort": {"total_vendido": -1}},
-    {"$limit": 1}
-])
-
+result = analyzer.ProdutoMaisVendido(db)
 writeAJson(result, "Produto mais vendido por compra")
 
-# 3 - cliente que mais gastou
-result = db.collection.aggregate([
-    {"$unwind": "$produtos"},
-    {"$group": {"_id": "$cliente_id", "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
-    {"$sort": {"total": -1}},
-    {"$limit": 1}
-])
-
+result = analyzer.OMaisRico(db)
 writeAJson(result, "Cliente que mais gastou")
 
-
-# 4 - Produtos com vendas acima de 1
-
-result = db.collection.aggregate([
-    {"$unwind": "$produtos"},
-    {"$group": {"_id": "$produtos.descricao", "vendas_maior_1": {"$sum": "$produtos.quantidade"}}},
-    {"$match": {"vendas_maior_1": {"$gt": 1}}},
-    {"$project": {"_id": 1}}
-])
-
-
+result = analyzer.ProdutosMaior1(db)
 writeAJson(result, "Produtos com vendas acima de 1")
 
---
+++ pipelines/productyAnalyser.py
@@ -0,0 +1,42 @@
+from database import Database
+
+class ProductAnalyzer:
+       def VendasPorDia(self,db: Database):
+        result = db.collection.aggregate([
+            {"$group": {"_id": "$data_compra", "vendas_por_dia": {"$sum":1}}},
+        ])
+        
+       
+        return result
+       
+       def ProdutoMaisVendido(self, db:Database):
+            result = db.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": "$produtos.descricao", "total_vendido": {"$sum": "$produtos.quantidade"}}},
+            {"$sort": {"total_vendido": -1}},
+            {"$limit": 1}
+            ])
+
+            return result
+       
+
+       def OMaisRico(self,db: Database):
+           result = db.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": "$cliente_id", "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+            {"$sort": {"total": -1}},
+            {"$limit": 1}
+            ])
+           
+           return result
+       
+
+       def ProdutosMaior1(self,db:Database):
+           result = db.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": "$produtos.descricao", "vendas_maior_1": {"$sum": "$produtos.quantidade"}}},
+            {"$match": {"vendas_maior_1": {"$gt": 1}}},
+            {"$project": {"_id": 1}}
+            ])
+           
+           return result
